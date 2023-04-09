326464607619f8c47395aae97c3b9fdd87a1ec49
dgladovic <rdomiran@gmail.com>
2023-04-09 23:32:12 +0200

    EXP 101

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer( (req,res) => {
-    console.log('request made');
+// const server = http.createServer( (req,res) => {
+//     console.log('request made');
 
-    console.log(req.url, req.method);
+//     console.log(req.url, req.method);
     
-    // res.setHeader('Content-type', 'text/html');
+//     // res.setHeader('Content-type', 'text/html');
     
-    if(req.url === '/home' ){
-        res.setHeader('Content-type', 'text/html');
-        res.write('<p>Koga</p>');
-        res.end();
-    }
-    else{
-        res.setHeader('Content-type', 'text/html');
-        res.write('<p>Nikoga!</p>');
-        res.end();
-    }
+//     if(req.url === '/home' ){
+//         res.setHeader('Content-type', 'text/html');
+//         res.write('<p>Koga</p>');
+//         res.end();
+//     }
+//     else{
+//         res.setHeader('Content-type', 'text/html');
+//         res.write('<p>Nikoga!</p>');
+//         res.end();
+//     }
     
+// });
+
+// server.listen(3000,'localhost', ()=> {
+//     console.log('listening for request');
+// })
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/home", function (req, res) {
+  res.send("Hello at my home!");
 });
 
-server.listen(3000,'localhost', ()=> {
-    console.log('listening for request');
-})
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+  });
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
